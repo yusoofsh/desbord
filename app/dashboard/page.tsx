32077b4653bf486c8ca92a88a4607b5399988b75
Yusoof Moh <18055365+yusoofsh@users.noreply.github.com>
2023-12-31 01:07:42 +0700

    feat: chapter seven

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,49 @@
-const DashboardPage = () => {
-  return <p>Dashboard Page</p>;
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "@/lib/data";
+import { Card } from "@/ui/dashboard/cards";
+import { LatestInvoices } from "@/ui/dashboard/latest-invoices";
+import { RevenueChart } from "@/ui/dashboard/revenue-chart";
+import { lusitana } from "@/ui/fonts";
+
+const DashboardPage = async () => {
+  const revenuePromise = fetchRevenue();
+  const latestInvoicesPromise = fetchLatestInvoices();
+  const cardDataPromise = fetchCardData();
+
+  const {
+    "0": revenue,
+    "1": latestInvoices,
+    "2": {
+      numberOfCustomers,
+      numberOfInvoices,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    },
+  } = await Promise.all([
+    revenuePromise,
+    latestInvoicesPromise,
+    cardDataPromise,
+  ]);
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" type="collected" value={totalPaidInvoices} />
+        <Card title="Pending" type="pending" value={totalPendingInvoices} />
+        <Card title="Total Invoices" type="invoices" value={numberOfInvoices} />
+        <Card
+          title="Total Customers"
+          type="customers"
+          value={numberOfCustomers}
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 };
 export default DashboardPage;
--- ui/dashboard/latest-invoices.tsx
+++ ui/dashboard/latest-invoices.tsx
@@ -1,9 +1,9 @@
-// import clsx from "clsx";
+import { clsx } from "clsx";
 
 import { LatestInvoice } from "@/lib/definitions";
 import { lusitana } from "@/ui/fonts";
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
-// import Image from "next/image";
+import Image from "next/image";
 
 export const LatestInvoices = ({
   latestInvoices,
@@ -18,27 +18,25 @@ export const LatestInvoices = ({
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: comment in this code when you get to this point in the course */}
-
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
-                key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  "flex flex-row items-center justify-between py-4",
                   {
-                    'border-t': i !== 0,
+                    "border-t": i !== 0,
                   },
                 )}
+                key={invoice.id}
               >
                 <div className="flex items-center">
                   <Image
-                    src={invoice.image_url}
-                    alt={`${invoice.name}'s profile picture`}
+                    alt={`${invoice.name}'s profile`}
                     className="mr-4 rounded-full"
-                    width={32}
                     height={32}
+                    src={invoice.image_url}
+                    width={32}
                   />
                   <div className="min-w-0">
                     <p className="truncate text-sm font-semibold md:text-base">
@@ -57,7 +55,7 @@ export const LatestInvoices = ({
               </div>
             );
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
