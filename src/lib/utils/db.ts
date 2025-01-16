7eaef8e49e801b24053248a8fc88e44e12334f84
Yusoof Moh <me@yusoofsh.id>
2025-01-16 14:37:27 +0700

    fix: build again

--- src/lib/utils/db.ts
+++ src/lib/utils/db.ts
@@ -3,21 +3,26 @@ import { drizzle } from "drizzle-orm/d1"
 import * as schema from "@/lib/utils/schema"
 
 /**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
+ * Cache the database connection.
+ * This avoids creating a new connection on every HMR update.
  */
-export let client: D1Database
-const globalForDb = globalThis as unknown as {
+const globalForDB = globalThis as unknown as {
   client?: D1Database
 }
 
-const createDBClient = () => {
-  client =
-    globalForDb.client ?? (getRequestContext().env as { DB: D1Database }).DB
+function getDBClient() {
+  let client
 
-  if (process.env.NODE_ENV === "development") globalForDb.client = client
+  if (process.env.NODE_ENV === "development") {
+    client =
+      globalForDB.client ?? (getRequestContext().env as { DB: D1Database }).DB
+  } else {
+    client = globalForDB.client ?? (process.env.DB as unknown as D1Database)
+  }
+
+  globalForDB.client = client
 
   return drizzle(client, { schema })
 }
 
-export default createDBClient()
+export default getDBClient()
