105e725f626c49d3530659ee13e9e213205f8aa8
Yusoof Moh <me@yusoofsh.id>
2025-01-16 13:15:04 +0700

    fix: real one for not top level

--- src/lib/utils/db.ts
+++ src/lib/utils/db.ts
@@ -6,14 +6,22 @@ import * as schema from "@/lib/utils/schema"
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-const globalForDB = globalThis as unknown as {
+export let client: D1Database
+const globalForDb = globalThis as unknown as {
   client?: D1Database
 }
 
-export default function getDbClient() {
-  const client = (getRequestContext().env as { DB: D1Database }).DB
+function createDbClient() {
+  if (!globalForDb.client) {
+    client = (getRequestContext().env as { DB: D1Database }).DB
 
-  if (process.env.NODE_ENV === "development") globalForDB.client = client
+    if (process.env.NODE_ENV === "development") globalForDb.client = client
+  } else {
+    client = globalForDb.client
+  }
 
   return drizzle(client, { schema })
 }
+
+const db = createDbClient()
+export default db
